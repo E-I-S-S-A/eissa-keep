672cb1b1df60bd91a1ce4621d2b3697207fef790
Eissa Faheem <eissafaheem@gmail.com>
2025-01-12 19:56:46 +0530

    feat: redirectToAccounts if not signed in ad returning afterwards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
+import useUserHook from './hooks/useUserHook';
 
 function App() {
 
+  const [isSignedIn, setIsSignedIn] = useState<boolean | null>(null);
+  const { getAndSetUser, redirectToAccounts } = useUserHook()
+
+  useEffect(() => {
+    getUser();
+  }, [])
+
+  const getUser = async () => {
+    try {
+      const user = await getAndSetUser();
+      setIsSignedIn(true)
+    } catch (error) {
+      redirectToAccounts();
+    }
+  };
+
   return (
-    <Dashboard />
+    <>
+      {
+        isSignedIn === true ?
+          <Dashboard />
+          : "Loading"
+      }
+    </>
   );
 }
 
--- src/hooks/useUserHook.ts
+++ src/hooks/useUserHook.ts
@@ -1,13 +1,16 @@
 import { User } from "../entities/User";
 
 const useUserHook = () => {
-    const ACCOUNTS_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/accounts`;
+    const ACCOUNTS_BACKEND_BASE_URL = `${process.env.REACT_APP_ACCOUNTS_BACKEND_BASE_URL}/accounts`;
+    const ACCOUNTS_CLIENT_BASE_URL = `${process.env.REACT_APP_ACCOUNTS_CLIENT_BASE_URL}`;
+    const KEEP_CLIENT_BASE_URL = `${process.env.REACT_APP_KEEP_CLIENT_BASE_URL}`;
+
     let currentUser: User | null = null;
 
     const getAndSetUser = async (): Promise<User | null> => {
         if (currentUser) return currentUser;
 
-        const result = await fetch(`${ACCOUNTS_BASE_URL}/get-user`, {
+        const result = await fetch(`${ACCOUNTS_BACKEND_BASE_URL}/get-user`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -24,9 +27,16 @@ const useUserHook = () => {
         throw new Error("Failed to get user");
     };
 
+    const redirectToAccounts = () => {
+        window.location.replace(
+            `${ACCOUNTS_CLIENT_BASE_URL}?next=${KEEP_CLIENT_BASE_URL}`
+        );
+    };
+
     return {
         getAndSetUser,
         currentUser,
+        redirectToAccounts,
     };
 };
 
