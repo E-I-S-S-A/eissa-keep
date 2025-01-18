13ed3a50ec073823021e812180936e9cbb602dd7
Eissa Faheem <eissafaheem@gmail.com>
2025-01-18 11:43:57 +0530

    feat: cards grid

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,38 @@
 import styles from "./Navbar.module.css"
 import KeepLogo from "./../../assets/keep-icon.svg"
+import { EissaButton, EissaInputField } from "react-reusable-elements";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+
+interface FormData {
+    search: string
+}
 
 const Navbar = () => {
+
+    const { register, handleSubmit, formState: { errors, touchedFields }, reset } = useForm<FormData>({
+        mode: "all",
+    });
+
+
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        console.log("hii")
+    };
+
+    const handleReset = () => {
+        reset();
+    };
 
 
     return <div className={styles.main_conatiner}>
         <div className={styles.brand}>
-        <img src={KeepLogo} alt="Eissa Keep" className={styles.brand_logo}/>
-        <div className={styles.brand_name}>EKeep</div>
+            <img src={KeepLogo} alt="Eissa Keep" className={styles.brand_logo} />
+            <div className={styles.brand_name}>EKeep</div>
 
         </div>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.search_form}>
+            <EissaInputField label="" name="search" placeholder="Search" register={register} keepSpaceForError={false} bg="var(--dark-grey)" fontColor="var(--white)"/>
+        </form>
     </div>
 }
 
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,7 +6,29 @@ const Dashboard = () => {
 
     return <>
         <Navbar />
-        <div className={styles.main_container}>kKe</div>
+        <div className={styles.main_container}>
+            <div className={styles.card_container}>
+
+                {
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => {
+                        return <div className={styles.card}>
+                            <div className={styles.title}>This is my title</div>
+
+                            <div className={styles.body}>
+
+                                {
+                                    index % 2 == 0 ?
+                                        index + "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum doloribus sint et error cum numquam perferendis quae qui tempora, odio libero dignissimos dolore enim praesentium molestias deserunt. Quisquam, unde? Numquam accusamus minima ex, consectetur officia neque dolore tempora, iste enim consequatur libero, pariatur totam laboriosam architecto soluta reprehenderit quisquam unde."
+                                        :
+                                        index + "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, eveniet."
+                                }
+                            </div>
+                        </div>
+                    })
+
+                }
+            </div>
+        </div>
 
     </>
 };
