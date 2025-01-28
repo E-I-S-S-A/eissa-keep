82904f47ddeb8fc6008995aaa8e20bbf3c32eb48
Eissa Faheem <eissafaheem@gmail.com>
2025-01-28 23:14:09 +0530

    feat: avatar click

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import styles from "./Navbar.module.css"
 import KeepLogo from "./../../assets/keep-icon.svg"
-import { EissaButton, EissaInputField } from "react-reusable-elements";
+import { EissaAvatar, EissaButton, EissaInputField } from "react-reusable-elements";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Keep } from "../../models/keep";
 import CloseIcon from "../../assets/close.svg"
+import useUserHook from "../../hooks/useUserHook";
+import { User } from "../../entities/User";
 
 type NavbarProps = {
     setAllKeeps: React.Dispatch<React.SetStateAction<Keep[]>>
@@ -20,6 +22,23 @@ const Navbar = (props: NavbarProps) => {
         mode: "all",
     });
     const { setAllKeeps } = props;
+    const { getAndSetUser } = useUserHook();
+    const [user, setUser] = useState<User | null>(null);
+    const ACCOUNTS_URL = process.env.REACT_APP_ACCOUNTS_CLIENT_BASE_URL;
+
+    useEffect(() => {
+        getUser();
+    }, [])
+
+    const getUser = async () => {
+        try {
+            const userFromServer = await getAndSetUser();
+            console.log(user)
+            setUser(userFromServer);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
     const onSubmit: SubmitHandler<FormData> = (data) => {
@@ -28,6 +47,10 @@ const Navbar = (props: NavbarProps) => {
             return prev.filter(item => (item.description.includes(keyWord) || item.title.includes(keyWord)))
         })
     };
+
+    const onAvatarClick = () => {
+        window.open(ACCOUNTS_URL, "_blank")
+    }
 
     const handleReset = () => {
         reset();
@@ -38,16 +61,18 @@ const Navbar = (props: NavbarProps) => {
         <div className={styles.brand}>
             <img src={KeepLogo} alt="Eissa Keep" className={styles.brand_logo} />
             <div className={styles.brand_name}>EKeep</div>
-
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.search_form}>
-            {
-                <div className={`${styles.clear} ${isValid && styles.visible}`}>
-                    <EissaButton bg="var(--dark-grey)" borderColor="var(--dark-grey)" padding={0} icon={CloseIcon} onClick={handleReset} />
-                </div>
-            }
-            <EissaInputField rules={{ required: true }} label="" name="searchToken" placeholder="Search" register={register} keepSpaceForError={false} bg="var(--dark-grey)" fontColor="var(--white)" />
-        </form>
+        <div className={styles.actions}>
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.search_form}>
+                {
+                    <div className={`${styles.clear} ${isValid && styles.visible}`}>
+                        <EissaButton bg="var(--dark-grey)" borderColor="var(--dark-grey)" padding={0} icon={CloseIcon} onClick={handleReset} />
+                    </div>
+                }
+                <EissaInputField rules={{ required: true }} label="" name="searchToken" placeholder="Search" register={register} keepSpaceForError={false} bg="var(--dark-grey)" fontColor="var(--white)" />
+            </form>
+            <EissaAvatar height={35} name={user?.firstName} img={user?.avatar} CTA={onAvatarClick} />
+        </div>
     </div>
 }
 
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,7 @@
 export interface User {
     userId?: string;
     firstName?: string;
+    avatar?: string;
     lastName?: string;
     email?: string;
     password?: string;
