9d7af278ced4f1c96e41ad56e54c67d991b917d5
Eissa Faheem <eissafaheem@gmail.com>
2025-01-31 19:36:38 +0530

    feat: search functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import { Keep } from "../../models/keep";
 import CloseIcon from "../../assets/close.svg"
 import useUserHook from "../../hooks/useUserHook";
 import { User } from "../../entities/User";
+import useKeepHook from "../../hooks/useKeepHook";
 
 type NavbarProps = {
     setAllKeeps: React.Dispatch<React.SetStateAction<Keep[]>>
@@ -18,13 +19,15 @@ interface FormData {
 
 const Navbar = (props: NavbarProps) => {
 
-    const { register, handleSubmit, formState: { errors, touchedFields, isValid }, reset } = useForm<FormData>({
+    const { register, handleSubmit, watch, formState: { errors, touchedFields, isValid }, reset } = useForm<FormData>({
         mode: "all",
     });
     const { setAllKeeps } = props;
     const { getAndSetUser } = useUserHook();
     const [user, setUser] = useState<User | null>(null);
     const ACCOUNTS_URL = process.env.REACT_APP_ACCOUNTS_CLIENT_BASE_URL;
+    const { getKeeps } = useKeepHook();
+    const searchToken = watch("searchToken");
 
     useEffect(() => {
         getUser();
@@ -40,19 +43,27 @@ const Navbar = (props: NavbarProps) => {
         }
     }
 
-
-    const onSubmit: SubmitHandler<FormData> = (data) => {
-        const keyWord = data.searchToken;
-        setAllKeeps(prev => {
-            return prev.filter(item => (item.description.includes(keyWord) || item.title.includes(keyWord)))
-        })
+    const onSubmit: SubmitHandler<FormData> = async (data) => {
+        try {
+            console.log(data)
+            const keeps = await getKeeps(1, 100, data.searchToken);
+            setAllKeeps(keeps);
+        } catch (error) {
+            console.log("Error")
+        }
     };
 
     const onAvatarClick = () => {
         window.open(ACCOUNTS_URL, "_blank")
     }
 
-    const handleReset = () => {
+    const handleReset = async () => {
+        try {
+            const keeps = await getKeeps(1, 100);
+            setAllKeeps(keeps);
+        } catch (error) {
+            console.log("Error")
+        }
         reset();
     };
 
@@ -65,11 +76,11 @@ const Navbar = (props: NavbarProps) => {
         <div className={styles.actions}>
             <form onSubmit={handleSubmit(onSubmit)} className={styles.search_form}>
                 {
-                    <div className={`${styles.clear} ${isValid && styles.visible}`}>
+                    <div className={`${styles.clear} ${searchToken?.length && styles.visible}`}>
                         <EissaButton bg="var(--dark-grey)" borderColor="var(--dark-grey)" padding={0} icon={CloseIcon} onClick={handleReset} />
                     </div>
                 }
-                <EissaInputField rules={{ required: true }} label="" name="searchToken" placeholder="Search" register={register} keepSpaceForError={false} bg="var(--dark-grey)" fontColor="var(--white)" />
+                <EissaInputField label="" name="searchToken" placeholder="Search" register={register} keepSpaceForError={false} bg="var(--dark-grey)" fontColor="var(--white)" />
             </form>
             <EissaAvatar height={35} name={user?.firstName} img={user?.avatar} CTA={onAvatarClick} />
         </div>
